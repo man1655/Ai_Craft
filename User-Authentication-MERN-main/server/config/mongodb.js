3fed99da10b55aee909ca6c10c3d96c6dca26d15
man1655 <manpatel1655@gmail.com>
2025-08-19 00:07:01 +0530

    cganges

--- User-Authentication-MERN-main/server/config/mongodb.js
+++ User-Authentication-MERN-main/server/config/mongodb.js
@@ -4,7 +4,11 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+await mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 };
 
 export default connectDB;
